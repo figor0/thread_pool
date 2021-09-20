0656d95b83455e1c88752d9824f1cbad0f349f3e
figoro <furinforjob@gmail.com>
2021-09-21 00:32:55 +0300

    prepare for parsing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,49 @@
 #include <iostream>
 #include <thread_pool.h>
+#include <string>
+#include <vector>
+
+namespace aft = abstract::from::thread;
+
+class ISubmiter
+{
+public:
+    virtual bool prepare(const std::string& description) = 0;
+    virtual void submit() = 0;
+};
+
+class ThreadPoolSubmiter: public ISubmiter
+{
+public:
+    ThreadPoolSubmiter(std::weak_ptr<aft::ThreadPool> thread_pool_sptr)
+        :
+    {
+
+    }
+private:
+    std::shared_ptr<aft::ThreadPool>
+};
+
+class SimpleDelimeter: public ISubmiter
+{
+public:
+    bool prepare(const std::string& description)
+    {
+
+    }
+};
+
+class SubmitersFactory
+{
+public:
+    bool registrate();
+private:
+};
+
+void initEnvironment()
+{
+
+}
 
 int main()
 {
